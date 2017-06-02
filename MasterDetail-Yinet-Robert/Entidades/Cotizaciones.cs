402d1f830102391aad78137b75e7b6202cf2896a
yinet Jaquez <yinet.Jaquez@hotmail.com>
2017-06-02 14:30:06 -0400

    clases

---
+++ MasterDetail-Yinet-Robert/Entidades/Cotizaciones.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDetail_Yinet_Robert.Entidades
+{
+    class Cotizaciones
+    {
+        [Key]
+        public int CotizacionId { get; set; }
+        public DateTime Fecha { get; set; }
+        public double Monto { get; set; }
+    }
+}
---
+++ MasterDetail-Yinet-Robert/Entidades/CotizacionesDetalle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDetail_Yinet_Robert.Entidades
+{
+    class CotizacionesDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int CotizacionId { get; set; }
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
